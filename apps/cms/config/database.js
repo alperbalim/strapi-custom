d77e52a43cc8655d3f7235a4aa49320387163ca5
alperbalim <mustafaalperbalim@gmail.com>
2026-02-23 12:11:56 +0300

    fix

--- apps/cms/config/database.js
+++ apps/cms/config/database.js
@@ -1,6 +1,6 @@
-import path from 'path';
+const path = require('path');
 
-export default ({ env }) => ({
+module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
